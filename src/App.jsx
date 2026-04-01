e781fea365eb2e0eed42fe7775c5784db690893e
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-04-01 13:39:22 +0600

    tailwind react pricing option

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+import NavBar from './Components/Navbar/NavBar'
+
+function App() {
+  
+
+  return (
+    <>
+
+  <header>
+    <NavBar></NavBar>
+    <DaisyNav></DaisyNav>
+  </header>
+
+    </>
+  )
+}
+
+export default App
